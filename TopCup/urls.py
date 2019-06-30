7e91f35c42f4e4fccc10712ac5caacaf7f7cb1c4
YerayZhao <627138326@qq.com>
2019-06-30 15:27:50 +0800

    比赛列表-显示

--- TopCup/urls.py
+++ TopCup/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import competition.views as CptList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('competitionlist/', CptList.CompetitionList)
 ]
--- competition/views.py
+++ competition/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from competition.models import Competition
 
 # Create your views here.
+def CompetitionList(request):
+    list = Competition.objects.all()
+    context = {}
+    context['list'] = list
+    return render(request,"../templates/CompetitionList.html", context)
