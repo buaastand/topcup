f8a0c591b5333abcbde219dd36e110f6b87eb2fd
oiiDawn <oii.myself@outlook.com>
2019-06-30 19:54:03 +0800

    post feature (v1.0)

--- competition/views.py
+++ competition/views.py
@@ -12,17 +12,21 @@ def CompetitionDetail(request):
 def CompetitionInit(request):
     return render(request, "../templates/CompetitionInit.html")
 
-@require_http_methods(["GET"])
+@require_http_methods(["POST"])
 def CompetitionFormPost(request):
     response = {}
     try:
-        competition_data = Competition(
-            title = request.GET.get('title'),
-            abstract = request.GET.get('abstract'),
-            detail = request.GET.get('detail'),
-            rule = request.GET.get('rule'),
-        )
-        competition_data.save()
+        cptdata = Competition(title = request.POST.get('title'),
+                                abstract = request.POST.get('abstract'),
+                                rule = request.POST.get('rule'),
+                                detail = request.POST.get('detail'),
+                                init_date = request.POST.get('init_date'),
+                                submit_end_date = request.POST.get('submit_end_date'),
+                                check_end_date = request.POST.get('check_end_date'),
+                                review_end_date = request.POST.get('review_end_date'),
+                                defense_end_date = request.POST.get('defense_end_date'),
+                                finish_date = request.POST.get('finish_date'))
+        cptdata.save()
         response['msg'] = 'success'
         response['error_num'] = 0
     except Exception as e:
