4da393d5fa68e6b91a20bd339e94db24ea4ebcba
oiiDawn <oii.myself@outlook.com>
2019-06-30 17:38:36 +0800

    WTF

--- TopCup/urls.py
+++ TopCup/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('competitiondetail/', cpt.CompetitionDetail),
     path('cptinit/', cpt.CompetitionInit),
+    path('cptformpost',cpt.CompetitionFormPost),
 ]
--- competition/views.py
+++ competition/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.core import serializers
+from django.http import JsonResponse
+from competition.models import Competition
+import json
 
 # Create your views here.
 def CompetitionDetail(request):
@@ -7,3 +12,21 @@ def CompetitionDetail(request):
 def CompetitionInit(request):
     return render(request, "../templates/CompetitionInit.html")
 
+@require_http_methods(["GET"])
+def CompetitionFormPost(request):
+    response = {}
+    try:
+        competition_data = Competition(
+            title = request.GET.get('title'),
+            abstract = request.GET.get('abstract'),
+            detail = request.GET.get('detail'),
+            rule = request.GET.get('rule'),
+        )
+        competition_data.save()
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+
+    return JsonResponse(response)
