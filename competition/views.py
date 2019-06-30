6e418065b4060a73eefafe54332921778a50b3cf
zyh1019179614 <1019179614@qq.com>
2019-06-30 15:29:48 +0800

    cptdetail 2.0

--- competition/views.py
+++ competition/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Competition
 # Create your views here.
 def CompetitionDetail(request):
-    return render(request,"../templates/CompetitionDetail.html")
+    if 'id' in request.GET:
+        cptDetail=Competition.objects.get(id= request['id'])
+        context = {'cptDetail':cptDetail}
+        return render(request,"../templates/CompetitionDetail.html",context)
