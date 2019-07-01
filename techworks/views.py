ca1fd848397e6b6355fd2f1249b4ede67c05cd88
VagrantSY <shixiaomao@buaa.edu.cn>
2019-07-01 20:18:44 +0800

    techwork submit finish

--- techworks/views.py
+++ techworks/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.views import View
 from django.contrib.auth.decorators import login_required
-from .models import WorkInfo
+from .models import WorkInfo, Appendix
 from django.db.models import Q
-from competition.models import CompetitionRegistration,Competition
+from competition.models import CompetitionRegistration, Competition
 from users.models import Student
+from django.db import transaction
+import datetime
+import json
+
 
 # Create your views here.
 def test(request):
     request.user.username
+
+
 # @login_required(login_url='/login/')
 # @method_decorator(login_required,name='dispatch')
 class TechWorkListView(View):
@@ -17,19 +23,21 @@ class TechWorkListView(View):
         # if request.GET.get('workid', None) is None:
         #     WorkInfo.objects.create()
         username = request.user.username
+        if username == "":
+            username = "16211086"
         registrationlist = CompetitionRegistration.objects.filter(Q(first_auth__user__username=username)
-                                                                 | Q(second_auth__user__username=username)
-                                                                 | Q(third_auth__user__username=username)
-                                                                 | Q(forth_auth__user__username=username)
-                                                                 | Q(fifth_auth__user__username=username)
-                                                                 )
+                                                                  | Q(second_auth__user__username=username)
+                                                                  | Q(third_auth__user__username=username)
+                                                                  | Q(forth_auth__user__username=username)
+                                                                  | Q(fifth_auth__user__username=username)
+                                                                  )
         worklist_origin = WorkInfo.objects.filter(registration__in=registrationlist)
         # work = WorkInfo.objects.get(work_id='1').wo
-        WORKTYPE_MAP={
+        WORKTYPE_MAP = {
             1: "科技发明制作",
             2: "调查报告和学术论文"
         }
-        FIELD_MAP={
+        FIELD_MAP = {
             1: "A",
             2: "B",
             3: "C",
@@ -41,14 +49,15 @@ class TechWorkListView(View):
         for work in worklist_origin:
             worklist_ret.append({
                 'work_id': work.work_id,
-                'first_author': work.registration.first_auth,
+                'first_author': work.registration.first_auth.name,
                 'title': work.title,
                 'work_type': WORKTYPE_MAP[work.work_type],
                 'field': FIELD_MAP[work.field],
-                'competition': work.registration.competition,
-                'submitted': work.submitted,
+                'competition': work.registration.competition.title,
+                'submitted': "已提交" if work.submitted else "暂存",
             })
-        return render(request, 'techwork_list.html', {'worklist':worklist_ret})
+
+        return render(request, 'techwork_list.html', {'worklist': worklist_ret})
 
 
 class TechWorkView(View):
@@ -56,17 +65,21 @@ class TechWorkView(View):
         work_id = request.GET.get('workid', None)
         comptition_id = request.GET.get('cptid', None)
         username = request.user.username
-        if username=="":
+        if username == "":
             username = "16211086"
         work = None
         company_ret = []
         if work_id is None:
             if comptition_id is not None:
-                registration = CompetitionRegistration.objects.create(first_auth=Student.objects.get(user__username=username),competition=Competition.objects.get(id=comptition_id))
+                registration = CompetitionRegistration.objects.create(
+                    first_auth=Student.objects.get(user__username=username),
+                    competition=Competition.objects.get(id=comptition_id))
                 registration.save()
                 work_cnt = WorkInfo.objects.count()
                 BASE_NUM = 10000
-                work = WorkInfo.objects.create(registration=registration,title="",detail="",innovation="",keywords="",avg_score=0,work_id=BASE_NUM+work_cnt,work_type=1,field=1)
+                work = WorkInfo.objects.create(registration=registration, title="", detail="", innovation="",
+                                               keywords="", avg_score=0, work_id=BASE_NUM + work_cnt, work_type=1,
+                                               field=1)
                 work.save()
             else:
                 return HttpResponse(status=400)
@@ -84,14 +97,76 @@ class TechWorkView(View):
                 company.append(registration.forth_auth)
             if registration.fifth_auth is not None:
                 company.append(registration.fifth_auth)
-            company_ret=[]
+            company_ret = []
             for auth in company:
-                company_ret.append({"stu_id":auth.stu_id,
+                company_ret.append({"stu_id": auth.stu_id,
                                     "name": auth.name,
                                     "degree": auth.degree,
                                     "phone": auth.phone,
-                                    "email":auth.user.email})
-        return render(request, 'submit_techwork.html', {'work':work,'company':company_ret})
+                                    "email": auth.user.email})
+        return render(request, 'submit_techwork.html', {'work': work, 'company': company_ret})
 
     def post(self, request):
+        try:
+            with transaction.atomic():
+                work_id = int(request.POST.get('work_id'))
+                work = WorkInfo.objects.get(work_id=work_id)
+                registration = work.registration
+                work.title = request.POST.get('title', work.title)
+                work.detail = request.POST.get('detail', work.detail)
+                work.innovation = request.POST.get('innovation', work.innovation)
+                work.keywords = request.POST.get('keywords', work.keywords)
+                work.work_type = int(request.POST.get('work_type', work.work_type))
+                work.field = int(request.POST.get("field", work.field))
+                work.submitted = True if int(request.POST.get("submitted", work.submitted)) == 1 else False
+                FILE_TYLE_MAP = {
+                    "document": 0,
+                    "photo": 1,
+                    "video": 2
+                }
+                for k, file in request.FILES.items():
+                    # file = file[0]
+                    filename = file._name
+                    filedata = file
+                    filetype = FILE_TYLE_MAP[file.field_name.split('_')[0]]
+                    filedate = datetime.date.today()
+                    Appendix.objects.create(filename=filename, appendix_type=filetype, upload_date=filedate,
+                                            file=filedata, work=work)
 
+                team = json.loads(request.POST.get('company_table')) if request.POST.get(
+                    'company_table') != "" else None
+                AUTH_MAP = {
+                    0: registration.second_auth,
+                    1: registration.third_auth,
+                    2: registration.forth_auth,
+                    3: registration.fifth_auth
+                }
+                if team:
+                    for order, auth in enumerate(team):
+                        AUTH_MAP[order] = Student.objects.get(stu_id=auth['stu_id'])
+
+                registration.save()
+                work.save()
+        except Exception as e:
+            print(e)
+            return HttpResponse(status=400)
+        return HttpResponse(status=200)
+
+        # file = request.FILES.get("file")
+        # dataPOST = json.loads(request.body)
+        # dataPOST['birthdate'] = dataPOST['birthdate'][0:10]
+        # dataPOST['enroll_time'] = dataPOST['enroll_time'][0:10]
+        # register_form = RegisterForm(dataPOST)
+        # if register_form.is_valid():
+        #     email = dataPOST.get('email', '')
+        #     if Student.objects.filter(user__email=email):
+        #         return render(request, "register.html", {"register_form": register_form, "msg": "该邮箱已注册"})
+        #     password = dataPOST.get('password', '')
+        #
+        #     # 创建BaseUser对象
+        #     baseuser = BaseUser()
+        #     baseuser.username = dataPOST.get('stu_id', '')
+        #     baseuser.password = make_password(password)
+        #     baseuser.type = 1
+        #     baseuser.email = email
+        #     baseuser.save()
