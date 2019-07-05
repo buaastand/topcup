2f4eb4e2aefbd8a577c36001ed7fd8bdc0d12f08
zyh1019179614 <1019179614@qq.com>
2019-07-05 18:22:57 +0800

    遴选答辩的作品

--- competition/views.py
+++ competition/views.py
@@ -12,12 +12,13 @@ from users.models import Student
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django import forms
-
+from django.db.models import Q
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
-from competition.models import Competition
-
+from competition.models import Competition,CompetitionRegistration
+from techworks.models import WorkInfo
+from django.forms.models import model_to_dict
 
 # Create your views here.
 def GetUserIdentitiy(request):
@@ -40,7 +41,6 @@ def GetUserIdentitiy(request):
 
 def CompetitionDetail(request):
     user_name, user_identity = GetUserIdentitiy(request)
-    print(GetUserIdentitiy(request))
     cptDetail=Competition.objects.get(id= request.GET['id'])
     context = {'cptDetail':cptDetail}
     temp=datetime.date.today()
@@ -61,6 +61,33 @@ def CompetitionDetail(request):
     context['status_type']=status_type
     context['useridentity'] = user_identity
     context['username'] = user_name
+
+    WORKTYPE_MAP = {
+        1: "科技发明制作",
+        2: "调查报告和学术论文"
+    }
+    FIELD_MAP = {
+        1: "A",
+        2: "B",
+        3: "C",
+        4: "D",
+        5: "E",
+        6: "F",
+    }
+    defenseWorkList = WorkInfo.objects.filter(Q(if_defense=1)&Q(registration__competition=cptDetail))
+    # defenseWorkList=WorkInfo.objects.filter(if_defense=1,registration)
+    defense_WorkList_ret = []
+    for i in defenseWorkList:
+        defense_WorkList_ret.append(
+            {
+                'work_id': i.work_id,
+                'title': i.title,
+                'work_type': WORKTYPE_MAP[i.work_type],
+                'field': FIELD_MAP[i.field],
+            }
+        )
+    context['defenseworklist']=defense_WorkList_ret
+
     return render(request,"../templates/CompetitionDetail.html",context)
 
 def CompetitionUpdate():
@@ -135,8 +162,6 @@ def DeleteCompetition(request):
     if request.method == 'POST':
         id = request.POST.get('id')
         # id= int(temp)
-        print(request)
-        print(id,type(id))
         try:
             Competition.objects.get(id=id).delete()
         except:
